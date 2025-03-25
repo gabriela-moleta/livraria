f618439553e9edfa16e889896fa4baf08998f496
Gabriela Moleta <gabriela.moleta@aluno.senai.br>
2025-03-25 11:52:47 -0300

    feat: lançamentos

--- livraria/src/app/page.jsx
+++ livraria/src/app/page.jsx
@@ -3,6 +3,7 @@ import Banner from './components/banner';
 import Footer from './components/footer';
 import SecaoCategories from './components/secao_categories';
 import Livros from './components/secao_livros';
+import Lancamentos from './components/secao_lancamentos';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Banner />
       <Livros />
       <SecaoCategories /> 
+      <Lancamentos />
       <Footer />
 
     </div>
